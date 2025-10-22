dc6b127a6c0cc2e7db323e7633ec77e80c762047
FranciscoMartinezDev <frncc951@gmail.com>
2025-10-22 14:04:39 -0300

    desarrollo menu

--- src/Pages/Menu/PublicMenu.tsx
+++ src/Pages/Menu/PublicMenu.tsx
@@ -1,6 +1,8 @@
 import { useMenuContext } from "@/Context/MenuContext";
-import { Box, Card } from "@chakra-ui/react";
+import { Box, Card, Flex, Heading } from "@chakra-ui/react";
 import type { FC } from "react";
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa6";
+
 import './Menu.scss';
 
 export const PublicMenu: FC = () => {
@@ -9,7 +11,26 @@ export const PublicMenu: FC = () => {
     return (
         <Box className="public-menu">
             <Box className="front-page">
-                
+                <Box className="business-data">
+                    <Flex className="data" align={'end'} gap={5}>
+                        <Box className="logo-container">
+
+                        </Box>
+                        <Heading as={"h1"} fontSize={30}>Negocio</Heading>
+
+                        <Flex marginLeft={'auto'} gap={5}>
+                            <Box className="social">
+                                <FaFacebook />
+                            </Box>
+                            <Box className="social">
+                                <FaInstagram />
+                            </Box>
+                            <Box className="social">
+                                <FaWhatsapp />
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </Box>
             </Box>
             <Card.Root className="card-menu">
                 <Card.Body>
